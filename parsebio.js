d62532ded25d5e281c4fee2a1ba5003ff669e89b
Keshav Rao <keshavrao250@gmail.com>
2015-02-21 17:32:30 -0800

    added csv. simple python server to parse information.

--- parsebio.js
+++ parsebio.js
@@ -7,8 +7,31 @@ parsebio.hello = function(){
 
 parsebio.get = function() {
 	
+// 	Papa.parse("http://www.cbioportal.org/webservice.do?cmd=getMutationData&genetic_profile_id=ov_tcga_mutations+ucec_tcga_mutations&gene_list=BRCA1", {
+// 	download: true,
+// 	complete: function(results) {
+// 		console.log(results);
+// 	}
+// });
 
+// 	Papa.parse("http://cbioportal.org/webservice.do?cmd=getCaseLists&cancer_study_id=gbm_tcga", {
+// 	download: true,
+// 	step: function(row) {
+// 		console.log("Row:", row.data);
+// 	},
+// 	complete: function() {
+// 		console.log("All done!");
+// 	}
+// });
+Papa.parse("./cancerdata.csv", {
+	download: true,
+	complete: function(results) {
+		console.log(results);
+	}
+	// rest of config ...
+})
 
-	return TSV.stringify("http://www.cbioportal.org/webservice.do?cmd=getMutationData&genetic_profile_id=ov_tcga_mutations+ucec_tcga_mutations&gene_list=BRCA1");
-
+	
 }
+
+parsebio.get();
